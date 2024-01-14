2416e909e4dadf19bf11b9446b6c80b77f49b29b
davidwalterm <david.walter.marinescu@gmail.com>
2024-01-14 21:25:20 +0200

    Added append() Function Example

---
+++ appendFunction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str = "GeeksFor";
+
+    str.append("Geeks");
+
+    cout << "Appended string: " << str << endl;
+
+    return 0;
+}
