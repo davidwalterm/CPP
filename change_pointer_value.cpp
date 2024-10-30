afc5e62f7c1bc7b7cf7e232193d123c309a12336
LAPTOP <david.walter.marinescu@gmail.com>
2024-10-30 21:46:37 +0200

    Added pointer examples

---
+++ change_pointer_value.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string food = "Pizza";
+    string * ptr = &food;
+
+    //Output the value of food
+    cout << food << "\n";
+
+    //Output the memory address of food and output its value
+    cout << &food << "\n";
+
+    //Access the memory address of food and output its value
+    cout << *ptr << "\n";
+
+    //Change the value of its pointer
+    *ptr = "Hamburger";
+
+    //Output the new value of the pointer
+    cout << *ptr << "\n";
+
+    //Output the new value of the food variable
+    cout << food << "\n";
+
+    return 0;
+}
